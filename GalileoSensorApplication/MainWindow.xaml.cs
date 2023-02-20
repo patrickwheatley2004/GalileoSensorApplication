e9d3293a94ab6751811c18a1f771793560126f40
PATRICK WHEATLEY <30053724@tafe.wa.edu.au>
2023-02-20 09:16:57 +0800

    Added comments.

--- GalileoSensorApplication/MainWindow.xaml.cs
+++ GalileoSensorApplication/MainWindow.xaml.cs
@@ -28,8 +28,10 @@ namespace GalileoSensorApplication
         private LinkedList<double> sensorBList = new LinkedList<double>();
 
 
+        // 4.13
         private void FillCBX(ComboBox cbx, int Min, int Max, int dft)
         {
+            // filling the combo box with numbers as a substitue for a NUD Box.
             cbx.Items.Clear();
             for (int i = Min; i <= Max; i++)
             {
@@ -45,8 +47,10 @@ namespace GalileoSensorApplication
             ReadData readData = new ReadData(); // Instance of "ReadData" from Galileo6.DLL
             sensorAList.Clear();
             sensorBList.Clear();
+            Random randy = new Random();
             for (int i = 0; i < MaxSize; i++) // A for loop to populate the LinkedLists with the data.
             {
+
                 sensorAList.AddFirst(readData.SensorA(int.Parse(cbxMu.Text), int.Parse(cbxSigma.Text))); // Adding the data to the LinkedLists by reading the data from the DLL. (mu, sigma)
                 sensorBList.AddFirst(readData.SensorB(int.Parse(cbxMu.Text), int.Parse(cbxSigma.Text)));
             }
@@ -101,14 +105,15 @@ namespace GalileoSensorApplication
             for (int i = 0; i < max; i++)
             {
                 min = i;
-                for (int j = i + 1; j < max; j++)
+                for (int j = i + 1; j < max; j++) 
                 {
-                    if (LL.ElementAt(j) < LL.ElementAt(min))
+                    if (LL.ElementAt(j) < LL.ElementAt(min)) // if j is less than min, min is equal to j.
                     {
                         min = j;
                     }
                 }
 
+                // SWAP.
                 LinkedListNode<double> currentMin = LL.Find(LL.ElementAt(min));
                 LinkedListNode<double> currentI = LL.Find(LL.ElementAt(i));
 
@@ -125,12 +130,12 @@ namespace GalileoSensorApplication
         // 4.12
         private void btnSelectionSort_Click(object sender, RoutedEventArgs e)
         {
-            Stopwatch sw = new Stopwatch();
+            Stopwatch sw = new Stopwatch(); // using a stopwatch to count how long the sort takes. 
             sw.Reset();
             sw.Start();
             SelectionSort(sensorAList);
             sw.Stop();
-            tbxSelectionSortTime.Text = sw.ElapsedTicks.ToString() + " Ticks.";
+            tbxSelectionSortTime.Text = sw.ElapsedTicks.ToString() + " Ticks."; // telling the user how long the sort took. 
             ShowAllSensorData();
             DisplayListBoxData(LBSensorA, sensorAList);
         }
@@ -143,14 +148,16 @@ namespace GalileoSensorApplication
         // 4.12
         private bool InsertionSort(LinkedList<double> LL)
         {
+            
             bool swapped = true;
             int max = NumberOfNodes(LL);
             for (int i = 0; i < max - 1; i++)
             {
                 for (int j = i + 1; j > 0; j--)
                 {
-                    if (LL.ElementAt(j - 1) > LL.ElementAt(j))
+                    if (LL.ElementAt(j - 1) > LL.ElementAt(j)) // checking if j-1 is more than j
                     {
+                        // SWAP.
                         LinkedListNode<double> current = LL.Find(LL.ElementAt(j));
                         LinkedListNode<double> previous = LL.Find(LL.ElementAt(j - 1));
                         var temp = previous.Value;
@@ -167,12 +174,12 @@ namespace GalileoSensorApplication
         // 4.8
         private void btnInsertionSort_Click(object sender, RoutedEventArgs e)
         {
-            Stopwatch sw = new Stopwatch();
+            Stopwatch sw = new Stopwatch(); // using a stopwatch to count how long the sort takes.
             sw.Reset();
             sw.Start();
             InsertionSort(sensorAList);
             sw.Stop();
-            tbxInsertionSortTime.Text = sw.ElapsedTicks.ToString() + " Ticks.";
+            tbxInsertionSortTime.Text = sw.ElapsedTicks.ToString() + " Ticks."; // displaying how long the sort tooks. 
             ShowAllSensorData();
             DisplayListBoxData(LBSensorA, sensorAList);
         }
@@ -180,6 +187,9 @@ namespace GalileoSensorApplication
         // 4.9
         private int BinarySearchIterative(LinkedList<double> LL, double SearchValue, int Minimum, int Maximum)
         {
+            // binary search.
+            // trying to match the mid to our searched for value. 
+            // using a loop to try match the mid to our search value. 
             while (Minimum <= Maximum - 1)
             {
                 int mid = (Minimum + Maximum) / 2;
@@ -206,18 +216,18 @@ namespace GalileoSensorApplication
         {
             if (SelectionSort(sensorAList))
             {
-                try
+                try // using a try-catch to parse the search value in the search textbox to an integer. 
                 {
                     double searchItem = Double.Parse(tbxSearchItemA.Text);
-                    Stopwatch sw = new Stopwatch();
+                    Stopwatch sw = new Stopwatch(); // a stopwatch to count how long the search takes.
                     sw.Reset();
                     sw.Start();
                     int value = BinarySearchIterative(sensorAList, searchItem, 0, NumberOfNodes(sensorAList));
                     sw.Stop();
-                    tbxBinarySearchIterative.Text = sw.ElapsedTicks.ToString() + " Ticks.";
+                    tbxBinarySearchIterative.Text = sw.ElapsedTicks.ToString() + " Ticks."; // displaying how long the search took.
                     DisplayListBoxData(LBSensorA, sensorAList);
-                    MessageBox.Show("Your search was found at index " + value.ToString() + " for Sensor A.", "Iterative Search Found A.", MessageBoxButton.OK, MessageBoxImage.Information);
-                    Highlight(value, LBSensorA, sensorAList);
+                    MessageBox.Show("Your search was found at index " + value.ToString() + " for Sensor A.", "Iterative Search Found A.", MessageBoxButton.OK, MessageBoxImage.Information); // displaying the search found information to the user.
+                    Highlight(value, LBSensorA, sensorAList); // highlighting the searched for value.
                 }
                 catch (Exception)
                 {
@@ -229,7 +239,9 @@ namespace GalileoSensorApplication
         // 4.10
         private int BinarySearchRecursive(LinkedList<double> LL, double SearchValue, int Minimum, int Maximum )
         {
-
+            // binary search.
+            // we are trying to match the mid to our search value. 
+            // instead of using a loop we are using a recursive method which means the method is called over and over until the mid is equal to our searched value. 
             if (Minimum< Maximum - 1)
             {
                 int mid = (Minimum + Maximum) / 2;
@@ -254,18 +266,18 @@ namespace GalileoSensorApplication
         {
             if (SelectionSort(sensorAList))
             {
-                try
+                try // using a try-catch to parse the item in the search textbox as an integer.
                 {
                     double searchItem = Double.Parse(tbxSearchItemA.Text);
-                    Stopwatch sw = new Stopwatch();
+                    Stopwatch sw = new Stopwatch(); // starting the stopwatch to count how long the search took.
                     sw.Reset();
                     sw.Start();
                     int value = BinarySearchRecursive(sensorAList, searchItem, 0, NumberOfNodes(sensorAList));
                     sw.Stop();
-                    tbxBinarySearchRecursive.Text = sw.ElapsedTicks.ToString() + " Ticks.";
+                    tbxBinarySearchRecursive.Text = sw.ElapsedTicks.ToString() + " Ticks.";// displaying how long the search took.
                     DisplayListBoxData(LBSensorA, sensorAList);
-                    MessageBox.Show("Your search was found at index " + value.ToString() + " for Sensor A.", "Recursive Search Found A.", MessageBoxButton.OK, MessageBoxImage.Information);
-                    Highlight(value, LBSensorA, sensorAList);
+                    MessageBox.Show("Your search was found at index " + value.ToString() + " for Sensor A.", "Recursive Search Found A.", MessageBoxButton.OK, MessageBoxImage.Information); // telling the user that their search was found.
+                    Highlight(value, LBSensorA, sensorAList); // highlighting the searched for value.
                 }
                 catch (Exception)
                 {
@@ -277,17 +289,17 @@ namespace GalileoSensorApplication
         // 4.11
         private void Highlight(int valueindex, ListBox listBox, LinkedList<double> LL)
         {
-            listBox.Items.Clear();
+            listBox.Items.Clear(); // clearing the listbox.
             foreach (double data in LL)
             {
-                listBox.Items.Add(data);
+                listBox.Items.Add(data); // adding the items back to the listbox. 
             }
 
             for (int i = valueindex - 2; i < valueindex + 3; i++)
             {
-                if (i > -1 && i < NumberOfNodes(LL))
+                if (i > -1 && i < NumberOfNodes(LL)) // checking if the index is below 0 or over the maximum.
                 {
-                    listBox.SelectedItems.Add(listBox.Items.GetItemAt(i));
+                    listBox.SelectedItems.Add(listBox.Items.GetItemAt(i)); // selecting the searched values + values close to the selected one.
                 }
                 
             }
@@ -301,6 +313,7 @@ namespace GalileoSensorApplication
 
         private void tbxSearchItemA_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
+            // clearing the textboxes whenever the mouse hovers over. 
             tbxSearchItemA.Text = "";
             tbxSearchItemB.Text = "";
         }
@@ -309,18 +322,18 @@ namespace GalileoSensorApplication
         {
             if (SelectionSort(sensorBList))
             {
-                try
+                try // using a try-catch to parse the item in the search textbox to an integer.
                 {
                     double searchItem = Double.Parse(tbxSearchItemB.Text);
-                    Stopwatch sw = new Stopwatch();
+                    Stopwatch sw = new Stopwatch(); // starting a stopwatch to count how long the search took. 
                     sw.Reset();
                     sw.Start();
                     int value = BinarySearchIterative(sensorBList, searchItem, 0, NumberOfNodes(sensorBList));
                     sw.Stop();
-                    tbxBinarySearchIterativeB.Text = sw.ElapsedTicks.ToString() + " Ticks.";
+                    tbxBinarySearchIterativeB.Text = sw.ElapsedTicks.ToString() + " Ticks."; // displaying the search.
                     DisplayListBoxData(LBSensorB, sensorBList);
-                    MessageBox.Show("Your search was found at index " + value.ToString() + " for Sensor B.", "Iterative Search Found B.", MessageBoxButton.OK, MessageBoxImage.Information);
-                    Highlight(value, LBSensorB, sensorBList);
+                    MessageBox.Show("Your search was found at index " + value.ToString() + " for Sensor B.", "Iterative Search Found B.", MessageBoxButton.OK, MessageBoxImage.Information); // telling the user that their sort was found.
+                    Highlight(value, LBSensorB, sensorBList); // highlighting the instances found in the listbox. 
                 }
                 catch (Exception)
                 {
@@ -333,18 +346,18 @@ namespace GalileoSensorApplication
         {
             if (SelectionSort(sensorBList))
             {
-                try
+                try // using a try-catch to parse the item in the search textbox to an integer.
                 {
                     double searchItem = Double.Parse(tbxSearchItemB.Text);
-                    Stopwatch sw = new Stopwatch();
+                    Stopwatch sw = new Stopwatch(); // starting a stopwatch to count how long the search took. 
                     sw.Reset();
                     sw.Start();
                     int value = BinarySearchRecursive(sensorBList, searchItem, 0, NumberOfNodes(sensorBList));
                     sw.Stop();
-                    tbxBinarySearchRecursiveB.Text = sw.ElapsedTicks.ToString() + " Ticks.";
+                    tbxBinarySearchRecursiveB.Text = sw.ElapsedTicks.ToString() + " Ticks."; // displaying the search.
                     DisplayListBoxData(LBSensorB, sensorBList);
-                    MessageBox.Show("Your search was found at index " + value.ToString() + " for Sensor B.", "Recursive Search Found B.", MessageBoxButton.OK, MessageBoxImage.Information);
-                    Highlight(value, LBSensorB, sensorBList);
+                    MessageBox.Show("Your search was found at index " + value.ToString() + " for Sensor B.", "Recursive Search Found B.", MessageBoxButton.OK, MessageBoxImage.Information); // telling the user that their sort was found.
+                    Highlight(value, LBSensorB, sensorBList); // highlighting the instances found in the listbox.
                 }
                 catch (Exception)
                 {
@@ -355,12 +368,12 @@ namespace GalileoSensorApplication
         // 4.12
         private void btnSelectionSortB_Click(object sender, RoutedEventArgs e)
         {
-            Stopwatch sw = new Stopwatch();
+            Stopwatch sw = new Stopwatch(); // starting the stopwatch to count how long the sort takes
             sw.Reset();
             sw.Start();
             SelectionSort(sensorBList);
             sw.Stop();
-            tbxSelectionSortTimeB.Text = sw.ElapsedTicks.ToString() + " Ticks.";
+            tbxSelectionSortTimeB.Text = sw.ElapsedTicks.ToString() + " Ticks."; // displaying how long the sort took.
             ShowAllSensorData();
             DisplayListBoxData(LBSensorB, sensorBList);
         }
@@ -368,14 +381,27 @@ namespace GalileoSensorApplication
         // 4.12
         private void btnInsertionSortB_Click(object sender, RoutedEventArgs e)
         {
-            Stopwatch sw = new Stopwatch();
+            Stopwatch sw = new Stopwatch(); // starting the stopwatch to count how long the sort takes.
             sw.Reset();
             sw.Start();
             InsertionSort(sensorBList);
             sw.Stop();
-            tbxInsertionSortTimeB.Text = sw.ElapsedTicks.ToString() + " Ticks.";
+            tbxInsertionSortTimeB.Text = sw.ElapsedTicks.ToString() + " Ticks."; // displaying how long the sort took.
             ShowAllSensorData();
             DisplayListBoxData(LBSensorB, sensorBList);
+        }
+
+
+        // 4.14
+        private void tbxSearchItemA_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        // 4.14
+        private void tbxSearchItemB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
